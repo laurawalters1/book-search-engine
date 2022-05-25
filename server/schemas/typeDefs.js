e2a5ae483f6eac3677a1109fc72cdd71fd9aa64e
Laura Walters <laura.walters1098@yahoo.com>
2022-05-25 19:52:17 +0100

    finishing type defs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,32 @@
 const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    bookCount: int!
+    savedBooks: [Book]
+}
+type Book {
+    bookId: int!
+    authors: [String!]
+    description: String!
+    title: String!
+    image: String!
+    link: Sting!
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
   type Query {
     me(_id: int!): User
   }
 
+ 
   type Mutation {
     createUser(username: String!, email: String!, password: String!): User
     loginUser(username: String!, email: String!, password: String!): Auth
